8f837168bfcc76d37eadcc9f63a022f632f9f981
bitkhushi <khushivachhani991@gmail.com>
2023-04-10 10:12:15 +0530

    variable

---
+++ js/variables.js
@@ -0,0 +1,57 @@
+// console.log("okiiiessss :)");
+
+// var x = 10;
+// var x = 20;
+// console.log(x); //20
+
+// var x;
+// console.log(x); // undifined
+
+// var x = 10;
+// var x;
+// console.log(x); // 10
+
+
+//Global scope
+// var x = 10 
+// {
+//     var x = 20; // local scope or block scope 
+// }
+// console.log(x);
+
+// const x = 10;
+// x = x + 10;
+// console.log(x); // error
+
+
+// const x ;
+// x = 10;
+// console.log(x); // error
+
+// const x = 10;
+// const x = 20;
+// console.log(x); // error
+
+// const x = 10;
+// {
+//     const x = 10;
+// }
+// console.log(x); // block scope
+
+// let x = 10;
+// let x = 20; // same name variables are not allowed
+
+
+// let x = 10;
+// {
+//     let x = 10;
+// }
+// console.log(x); // block scope
+
+// let x; // declaration or defined
+// x = 2; // initialazation
+// console.log(x); 
+
+// let x = 10;
+// x = x+2;
+// console.log(x); // change the value of variable
